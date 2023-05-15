6acb77bfbb07db71c693f492b7f4bfb437b85a96
Bianca Gomes <biancagothicg@gmail.com>
2023-05-15 15:51:16 -0300

    first commit

---
+++ src/classe/DataMain.java
@@ -0,0 +1,15 @@
+package classe;
+
+public class DataMain {
+
+	public static void main(String[] args) {
+		Data d1 = new Data();
+		d1.dia = 15;
+		d1.mes = 05;
+		d1.ano = 2023;
+
+		System.out.println(d1.dia + "/" + d1.mes + "/" + d1.ano);
+
+	}
+
+}
